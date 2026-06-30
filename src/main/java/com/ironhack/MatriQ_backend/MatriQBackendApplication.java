cd0c7698282cd7f5ab702347a05e052d1e6b9c44
Nihad Bagirzade <nihad.bagirzade888@gmail.com>
2026-06-30 15:18:20 +0400

    feat: project initialization

---
+++ src/main/java/com/ironhack/MatriQ_backend/MatriQBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.MatriQ_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriQBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriQBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/MatriQ_backend/MatriQBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.MatriQ_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatriQBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
