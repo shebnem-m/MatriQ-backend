d2b5715d46304e33248442df0bb88847e453d636
Shebnem.M <snmmrdv@gmail.com>
2026-07-01 16:09:45 +0400

    Add UserRepository with custom queries

---
+++ src/main/java/com/ironhack/MatriQ_backend/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.ironhack.MatriQ_backend.repository;
+import com.ironhack.MatriQ_backend.entity.Role;
+import com.ironhack.MatriQ_backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    List<User> findByRole(Role role);
+}
