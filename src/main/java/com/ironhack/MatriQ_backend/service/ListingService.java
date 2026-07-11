a02c74333452d4f23ba8d5d57d4c7460da266cba
yagutrslbyl <yaguter-bpa202@code.edu.az>
2026-07-11 18:20:21 +0400

    Implement listing price sorting

--- src/main/java/com/ironhack/MatriQ_backend/service/ListingService.java
+++ src/main/java/com/ironhack/MatriQ_backend/service/ListingService.java
@@ -5,13 +5,42 @@ import com.ironhack.MatriQ_backend.dto.listing.ListingFilterDto;
 import com.ironhack.MatriQ_backend.dto.listing.ListingResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
 import java.util.UUID;
 
+
 public interface ListingService {
-    ListingResponseDto createListing(ListingCreateDto dto);
-    Page<ListingResponseDto> getAllListings(Pageable pageable);
-    ListingResponseDto getListingById(UUID id);
-    ListingResponseDto updateListing(UUID id, ListingCreateDto dto);
-    void deleteListing(UUID id);
-    Page<ListingResponseDto> searchListings(ListingFilterDto filterDto, Pageable pageable);
+
+
+    ListingResponseDto createListing(
+            ListingCreateDto dto
+    );
+
+
+    Page<ListingResponseDto> getAllListings(
+            Pageable pageable
+    );
+
+
+    ListingResponseDto getListingById(
+            UUID id
+    );
+
+
+    ListingResponseDto updateListing(
+            UUID id,
+            ListingCreateDto dto
+    );
+
+
+    void deleteListing(
+            UUID id
+    );
+
+
+    Page<ListingResponseDto> searchListings(
+            ListingFilterDto filterDto,
+            Pageable pageable
+    );
+
 }
